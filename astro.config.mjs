e000ce236446c8bcd5453adb111b5a8da08a4f2d
Diego Breskovit <diegobresko@gmail.com>
2023-03-23 16:17:14 -0300

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [compress(), mdx(), tailwind()],
-})
+  output: "server",
+  adapter: vercel()
+});
