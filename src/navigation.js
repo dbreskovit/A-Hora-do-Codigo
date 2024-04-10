3c79f7b220ad99dce51aa34bfb08dcfd69001d9c
Diego Breskovit <81165451+dbreskovit@users.noreply.github.com>
2024-04-10 15:48:30 -0300

    configure deploy

--- src/navigation.js
+++ src/navigation.js
@@ -10,91 +10,110 @@ export const headerData = {
       text: 'Site',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: 'A Proposta',
+          href: getPermalink('/#projeto'),
         },
         {
-          text: 'Services',
-          href: getPermalink('/services'),
+          text: 'Monitorias',
+          href: getPermalink('/#monitorias'),
         },
         {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
+          text: 'Oficinas',
+          href: getPermalink('/#oficinas'),
         },
         {
-          text: 'About us',
+          text: 'Sobre Nós',
           href: getPermalink('/about'),
         },
         {
-          text: 'Contact',
-          href: getPermalink('/contact'),
+          text: 'FAQs',
+          href: getPermalink('/#faqs'),
         },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
+        // {
+        //   text: 'Services',
+        //   href: getPermalink('/services'),
+        // },
+        // {
+        //   text: 'Pricing',
+        //   href: getPermalink('/pricing'),
+        // },
+
+        // {
+        //   text: 'Contact',
+        //   href: getPermalink('/contact'),
+        // },
+        // {
+        //   text: 'Terms',
+        //   href: getPermalink('/terms'),
+        // },
+        // {
+        //   text: 'Privacy policy',
+        //   href: getPermalink('/privacy'),
+        // },
       ],
     },
     {
       text: 'Oficinas',
       links: [
         {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
+          text: 'Oficina Python',
+          //href: getPermalink('/landing/lead-generation'),
+          href: getPermalink('#'),
         },
         {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
+          text: 'Oficina React',
+          //href: getPermalink('/landing/sales'),
+          href: getPermalink('#'),
         },
         {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
+          text: 'Oficina Node.js',
+          //href: getPermalink('/landing/click-through'),
+          href: getPermalink('#'),
         },
         {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
+          text: 'Oficina Angular',
+          //href: getPermalink('/landing/product'),
+          href: getPermalink('#'),
         },
         {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
+          text: 'Oficina PHP',
+          //href: getPermalink('/landing/pre-launch'),
+          href: getPermalink('#'),
         },
         {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
+          text: 'Oficina Javascript',
+          //href: getPermalink('/landing/subscription'),
+          href: getPermalink('#'),
         },
       ],
     },
     {
-      text: 'Posts',
+      text: 'Blog',
       links: [
         {
-          text: 'Blog List',
+          text: 'Todos os Posts',
           href: getBlogPermalink(),
         },
         {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
+          text: 'Categoria',
           href: getPermalink('tutorials', 'category'),
         },
         {
-          text: 'Tag Page',
+          text: 'Tags',
           href: getPermalink('astro', 'tag'),
         },
+        // {
+        //   text: 'Article',
+        //   href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+        // },
+        // {
+        //   text: 'Article (with MDX)',
+        //   href: getPermalink('markdown-elements-demo-post', 'post'),
+        // },
       ],
     },
   ],
-  //actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  //actions: [{ text: 'Horários', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
@@ -145,14 +164,14 @@ export const footerData = {
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+     { text: 'Termos de Uso', href: getPermalink('/terms') },
+     { text: 'Privacidade', href: getPermalink('/privacy') },
   ],
   socialLinks: [
     // { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
     // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
   footNote: ` © ` + new Date().getFullYear() + ` · A Hora do Código · Todos os direitos reservados.`,
